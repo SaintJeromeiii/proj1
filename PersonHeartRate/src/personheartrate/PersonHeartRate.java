a47d50379452a93f69c456097b05fcb7828f47b0
SaintJeromeiii <91078423+SaintJeromeiii@users.noreply.github.com>
2024-09-21 07:49:45 -0400

    Add files via upload
    Testing to see if I can open this program on my Linux and/or OS system.

---
+++ PersonHeartRate/src/personheartrate/PersonHeartRate.java
@@ -0,0 +1,46 @@
+package personheartrate;
+
+import java.util.Scanner;
+public class PersonHeartRate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Let's find your heart rate!");
+        System.out.println();
+        
+        Scanner sc = new Scanner(System.in);
+        
+        
+        //get input from user
+        System.out.println("Enter your first name: ");
+       
+        String name0 = sc.nextLine();
+        
+        System.out.println("Enter your last name: ");
+        String name1 = sc.nextLine();
+        
+        System.out.println("What is your date of birth?");
+        String name2 = sc.nextLine();
+        
+        //get age from date of birth
+        
+        //calculate heart rate
+        
+        //format and display the result
+        
+        
+        //using my string variables I created/
+        System.out.println("Hi " + name0 + " " + name1 + "! Your age is "+ name2 + ".");
+        
+//        //prompt user to continue
+//        System.out.print(s:continue? (y/n):);
+//        choice = sc.nextLine();
+//        System.out.println();
+        
+       
+        
+    }//end main
+    
+}
